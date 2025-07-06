71e0be16e2b8db96b2e62dd5d538109b8d99cf04
Nithish <mailtonithishkumar4@gmail.com>
2025-07-06 22:01:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+
 import streamlit as st
-import cv2
+from streamlit_drawable_canvas import st_canvas
 import numpy as np
 from PIL import Image
+import pandas as pd
 from utils import load_colors, get_closest_color_name
 
 st.set_page_config(page_title="Color Detector", layout="centered")
@@ -12,37 +14,33 @@ colors_df = load_colors()
 uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
 if uploaded_file:
-    image = Image.open(uploaded_file)
+    image = Image.open(uploaded_file).convert("RGB")
     image_np = np.array(image)
 
-    st.image(image, caption="Click on the image to detect color", use_column_width=True)
+    st.subheader("Click on the image to detect color:")
+    canvas_result = st_canvas(
+        fill_color="rgba(0, 0, 0, 0)",
+        stroke_width=0,
+        background_image=image,
+        update_streamlit=True,
+        height=image_np.shape[0],
+        width=image_np.shape[1],
+        drawing_mode="point",
+        key="canvas",
+    )
 
-    click = st.image(image_np, use_column_width=True)
+    if canvas_result.json_data and len(canvas_result.json_data["objects"]) > 0:
+        last_obj = canvas_result.json_data["objects"][-1]
+        x, y = int(last_obj["left"]), int(last_obj["top"])
 
-    # Capture mouse clicks using OpenCV window (for local dev only)
-    def click_event(event, x, y, flags, param):
-        if event == cv2.EVENT_LBUTTONDOWN:
-            b, g, r = image_np[y, x]
-            color_name = get_closest_color_name(r, g, b, colors_df)
-            st.session_state['selected_color'] = {
-                'rgb': (r, g, b),
-                'name': color_name
-            }
+        r, g, b = image_np[y, x]
+        color_name = get_closest_color_name(r, g, b, colors_df)
 
-    if st.button("Activate Click Detection"):
-        cv2.imshow("Image - Click to detect color", cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
-        cv2.setMouseCallback("Image - Click to detect color", click_event)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-    if 'selected_color' in st.session_state:
-        r, g, b = st.session_state['selected_color']['rgb']
-        name = st.session_state['selected_color']['name']
-        st.markdown(f"**Detected Color:** `{name}`")
+        st.markdown(f"**Color Name:** `{color_name}`")
         st.markdown(f"**RGB:** `{r}, {g}, {b}`")
         st.markdown(
             f"<div style='width:100px; height:100px; background-color:rgb({r},{g},{b}); border:1px solid #000;'></div>",
             unsafe_allow_html=True
         )
 else:
-    st.info("Please upload an image to start color detection.")
+    st.info("Upload an image to get started.")
